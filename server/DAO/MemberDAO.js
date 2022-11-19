d57fc2932e8558df7307dea0985997eb43031589
neneong <pukkuk12@naver.com>
2022-11-19 21:55:30 +0900

    Update MemberDAO.js

--- server/DAO/MemberDAO.js
+++ server/DAO/MemberDAO.js
@@ -4,43 +4,48 @@ const exception = require('../Exception')
 const con = dbcon.getPool();
 
 module.exports = class {
-  async getMember(memberId) {
+
+  //사용자 가져오는 함수
+  getMember(memberId) {
     try {
-      con.getConnection(function (err, connection){
+      con.getConnection(function (err, connection) {
         if (err) {
           console.error("err : " + err);
           return next(err);
         }
-        sql = 'select * from member where memberid='+memberId;
-        connection.query(sql, function (err, rows) {
+        sql = 'select * from member where memberid=?';
+        connection.query(sql, memberId, function (err, rows) {
           if (err) {
             console.error("err : " + err);
             return next(err);
           }
           console.log("rows : " + JSON.stringify(rows));
-    
+
           req.db_result = rows;
           connection.release();
-    
+
           next();
         })
       })
+      return rows
     } catch (err) {
       exception.addThrow(new Exception(ErrorCode, err.message))
       return null
     }
   }
 
-  // 멤버 추가함수
+  // 사용자 추가함수
+  // 성공시 true, 실패시 false로 리턴.
   createMember = async (memberId, memberPwd, memberName) => {
     try {
-      con.getConnection(function (err, connection){
+      con.getConnection(function (err, connection) {
         if (err) {
           console.error("err : " + err);
           return next(err);
         }
-        sql = 'insert into from member values("'+memberId+'","'+memberPwd+'","'+memberName+"'";
-        connection.query(sql, function (err) {
+        let sql = 'insert into member set ?';
+        let data = { memberId: memberId, memberPwd: memberPwd, memberName: memberName }
+        connection.query(sql, data, function (err) {
           if (err) {
             console.error("err : " + err);
             return next(err);
@@ -48,27 +53,73 @@ module.exports = class {
 
           req.db_result = rows;
           connection.release();
-    
+
           next();
         })
       })
-    } catch (err) {
-      exception.addThrow(new Exception(ErrorCode, err.message))
-      return null
-    }
-  }
-
-  // 관리자가 유저를 영원히 삭제하고 싶을때 쓰는 함수
-  async deleteClassForever(classId) {
-    try {
-      return (
-        (await Class.destroy({
-          where: { classId },
-        })) !== null
-      )
+      return true
     } catch (err) {
       exception.addThrow(new Exception(ErrorCode, err.message))
       return false
+    }
+  }
+
+  //사용자 비밀번호 변경함수
+  // true, false로 반환.
+  modifyMemberPassword(memberId, memberPwd) {
+    try {
+      con.getConnection(function (err, connection) {
+        if (err) {
+          console.error("err : " + err);
+          return next(err);
+        }
+        let sql = 'update member set memberpwd=? where memberid=?';
+        let data = [memberPwd, memberId]
+        connection.query(sql, data, function (err) {
+          if (err) {
+            console.error("err : " + err);
+            return next(err);
+          }
+
+          req.db_result = rows;
+          connection.release();
+
+          next();
+        })
+      })
+      return true
+    } catch (err) {
+      exception.addThrow(new Exception(ErrorCode, err.message))
+      return false
+    }
+  }
+
+  // 사용자 삭제 함수
+  // true, false로 반환.
+  deleteMember(memberId) {
+    try {
+      con.getConnection(function (err, connection) {
+        if (err) {
+          console.error("err : " + err);
+          return next(err);
+        }
+        sql = 'delete member where memberid=?';
+        connection.query(sql, memberId, function (err) {
+          if (err) {
+            console.error("err : " + err);
+            return next(err);
+          }
+
+          req.db_result = rows;
+          connection.release();
+
+          next();
+        })
+      })
+      return true;
+    } catch (err) {
+      exception.addThrow(new Exception(ErrorCode, err.message))
+      return false;
     }
   }
 
