614620679a5aa06ed60c10fd437d9ec2cd0b64b3
neneong <pukkuk12@naver.com>
2022-11-22 17:01:35 +0900

    update Logout

--- server/Controller/MemberController/MemberLogoutController.js
+++ server/Controller/MemberController/MemberLogoutController.js
@@ -19,13 +19,25 @@ module.exports = class MemberLogoutController extends Controller {
         }
 
         //임시
-        const info = true
-
+        let info = false;
+        
+        try {
+            res.clearCookie('member_id');
+            req.session.destroy(function(){
+                req.session;
+                info = true;
+            }); 
+          } catch (error) {
+            console.log(error);
+          }
+          
         if (info) {
             this.sendResponse(true, 200, {message:'로그아웃 성공'}, res);
         } else {
             this.sendResponse(false, 404, {message:'로그아웃 실패'}, res)
         }
+
+        
     }
     
     
