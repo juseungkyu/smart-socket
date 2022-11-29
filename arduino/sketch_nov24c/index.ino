65adb466ee9b963b3f204a6d8331352cccf3bcaa
neneong <63086504+neneong@users.noreply.github.com>
2022-11-29 10:20:31 +0900

    update file

--- arduino/sketch_nov24c/index.ino
+++ arduino/sketch_nov24c/index.ino
@@ -1,9 +1,70 @@
+const char* ssid = "test_ap";
+const char* password = "1q2w3e4r!";
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
 
-}
+  Serial.println();
+  Serial.print("CONNECTING TO ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password); // 와이파이 이름과 비밀번호를 통해 WIFI연결을 시작하겠다 // WL_CONNECTED라는 값을 돌려준다
+
+  while(WiFi.status() != WL_CONNECTED){ // 네트워크의 연결 상태, 8개의 리턴값 / STATUS와 WL_CONNECTED 값이 같은지를 통해 제대로 연결이 되있는지를 확인할 수 있다
+    delay(500);
+    Serial.print(".");
+    
+  }
+  Serial.println();
+  Serial.println("Wifi connected!");
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
 
+        if (c == '\n' && currentLineIsBlank) {
+          Serial.println("Sending response");
+
+          client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"  
+            "Refresh: 20\r\n"     
+            "\r\n");
+          client.print("<!DOCTYPE HTML>\r\n");
+          client.print("<html>\r\n");
+          client.print("<h1>Hello World!</h1>\r\n");
+          client.print("Requests received: ");
+          client.print(++reqCount);
+          client.print("<br>\r\n");
+          client.print("Analog input A0: ");
+          client.print(analogRead(0));
+          client.print("<br>\r\n");
+          client.print("</html>\r\n");
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+
+    delay(10);
+
+    client.stop();
+    Serial.println("Client disconnected");
+  }
 }
