dfd45931e5f736ae0b16bf3aa4f41080014e3e9d
neneong <pukkuk12@naver.com>
2022-12-05 23:41:26 +0900

    update app structure

---
+++ ios/smart-socket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smart-socket
+//
+//  Created by 정수민 on 2022/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
